104368e902b9d2aef89ddc219805dfde4b7a5dfe
Amber <108214275+Altreeee@users.noreply.github.com>
2024-03-17 21:15:30 +0800

    Add files via upload

---
+++ Yolo/Auto_test_onpc.py
@@ -0,0 +1,136 @@
+# -*- coding:utf-8 -*-
+"""
+
+run image---->angle
+
+get data from camera:/dev/video0   
+
+params:vels=1535
+
+put image to serial :/dev/ttACM0
+
+
+"""
+
+
+import select
+from ctypes import *
+import numpy as np
+import cv2 
+from sys import argv
+
+
+#import paddle.fluid as fluid
+from PIL import Image
+
+
+
+
+import test
+
+
+#script,vels,save_path= argv
+
+'''def dataset(video): 
+    lower_hsv = np.array([19,20,210])
+    upper_hsv = np.array([40, 255, 255])
+
+    select.select((video,), (), ())        
+    image_data = video.read_and_queue()
+    
+    frame = cv2.imdecode(np.frombuffer(image_data, dtype=np.uint8), cv2.IMREAD_COLOR)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask0 = cv2.inRange(hsv, lowerb=lower_hsv, upperb=upper_hsv)#在上下限范围内的为白色，不符合的为黑色
+    #mask1 = cv2.inRange(hsv, lowerb=lower_hsv1, upperb=upper_hsv1)
+    mask = mask0 #+ mask1
+
+
+    img = Image.fromarray(mask)
+    img = img.resize((120, 120), Image.ANTIALIAS)
+    img = np.array(img).astype(np.float32)
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    img = img.transpose((2, 0, 1))
+    img = img[(2, 1, 0), :, :] / 255.0
+    img = np.expand_dims(img, axis=0)   
+    return img
+'''
+
+def dataset(video):
+    select.select((video,), (), ())        
+    image_data = video.read_and_queue()
+    # 将图像数据解码为OpenCV格式
+    frame = cv2.imdecode(
+        np.frombuffer(image_data, dtype=np.uint8), 
+        cv2.IMREAD_COLOR
+    )
+    '''cv2.imwrite(f'frame_{frame_number}.jpg', frame)'''
+    return frame
+
+
+
+if __name__ == "__main__":
+    cout = 0
+    #cam = cv2.VideoCapture(0)   #打开编号为00的摄像头【0号是上面的摄像头】
+    # 从当前目录中读取 "test.jpg" 图片
+    image_path = "0.jpg"
+    
+
+
+    vels  = 1545
+    '''lib_path = path + "/lib" + "/libart_driver.so"'''
+    #lib_path = "./lib/libart_driver.so"
+    #so = cdll.LoadLibrary
+    #lib = so(lib_path)
+    #car = "/dev/ttyACM0"
+    #print("reach line 67")
+    
+    try:
+        # 读取图像
+        frame = cv2.imread(image_path)
+        vel = int(vels)
+        # 确保成功读取图像
+        if frame is not None:
+            # 调整图像大小
+            res = cv2.resize(frame, (224, 224), interpolation=cv2.INTER_CUBIC)
+            # 保存调整大小后的图像
+            cv2.imwrite("1.jpg", res)
+
+
+            
+            
+        image_path = "1.jpg"
+		    
+        print("reach line 72")
+        #0: 'person', 1: 'crossing', 2: 'stop', 3: 'Left', 4: 'Straight', 5: 'Right'
+        a = 1500
+        n_labels = test.infer(image_path)
+        print(n_labels)
+        if 2 in n_labels:
+            print("找到了名为'停止'的标签！")
+            vel = 1500
+        if 0 in n_labels:
+            print("找到了名为'人'的标签！")
+            vel = 1500
+        if 1 in n_labels:
+            print("找到了名为'斑马线'的标签！")
+            vel = 1500
+        if 3 in n_labels:
+            print("找到了名为'左'的标签！")
+            a = 1650
+        if 5 in n_labels:
+            print("找到了名为'右'的标签！")
+            a = 1350
+
+        print("end of send_data")
+
+        #execfile('//home//deep//paddlepaddle//pd_6//test.py')
+        #lib.send_cmd(vel, a)
+        print(cout)
+        cout=cout+1
+        print("angle: %d, throttle: %d" % (a, vel))
+
+    except:
+        print('error')
+    
+
+
